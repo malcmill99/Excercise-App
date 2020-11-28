23f180cf94a689fcdb1ed03d3c5e8c75337a05b7
Malcolm Miller <malcmill99@gmail.com>
2020-11-28 14:37:36 -0500

    About page info
    more information on my about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="about">
     <h1 class="title">This is an about page</h1>
-    <p>Here is where you can find information about this exercise application</p>
+    <p>Here is where you can find information about this exercise application.</p>
+    <p>
+      This app is supposed to let users log in, input 
+      an exercise, and share that information with 
+      their friends!
+    </p>
+    <p>
+      This app will help people to keep track of what
+      workouts they are doing, where they are doing them,
+      how long they are working out for, and some more 
+      information.
+    </p>
   </div>
 
 </template>
