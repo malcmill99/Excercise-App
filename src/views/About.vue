06c48dfc45031597fc20ac8f915bcc1094da726f
Malcolm Miller <malcmill99@gmail.com>
2020-11-29 13:19:37 -0500

    Front end progress
    Pictures and info for the admin and about pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,24 @@
   <div class="about">
     <h1 class="title">This is an about page</h1>
     <p>Here is where you can find information about this exercise application.</p>
+    <div>
+      <img alt="Vue logo" src="../assets/friends.png" />
+    </div>
+    <div>
     <p>
       This app is supposed to let users log in, input 
       an exercise, and share that information with 
       their friends!
     </p>
+    </div>
+    <div>
     <p>
       This app will help people to keep track of what
       workouts they are doing, where they are doing them,
       how long they are working out for, and some more 
       information.
     </p>
+    </div>
   </div>
 
 </template>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,10 @@
 <template>
 
-
   <div class="admin">
     <h1 class="title">This is an admin page</h1>
-    <p>Here is where an admin would be redirected after logging in</p>
-
+    <p>Here is where an admin would be redirected after logging in!</p>
+    <img alt="Vue logo" src="../assets/administrator.png" />
   </div>
-  
   
 
 </template>
