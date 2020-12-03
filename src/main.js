711363cf10847f993e02b8184ac9691b59009ec5
Malcolm Miller <malcmill99@gmail.com>
2020-12-03 14:17:36 -0500

    User lookup with buefy
    Attempted to make a user lookup page that searched through the database tables for user information

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import Buefy from 'buefy'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false;
+Vue.use(Buefy)
 
 new Vue({
   router,
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,55 @@
 <template>
-  <div class="admin">
-    <h1 class="title">This is an admin page</h1>
-    <p>Here is where an admin would be redirected after logging in!</p>
-    <img alt="Vue logo" src="../assets/administrator.png" />
-  </div>
-  
+    <div class ="admin">
+        <p class="content"><b>User Selected:</b> {{ selected }}</p>
+        <b-field label="Find a user from the database">
+            <b-autocomplete
+                rounded
+                v-model="name"
+                :data="filteredDataArray"
+                placeholder="e.g. Bruno Mars"
+                icon="magnify"
+                clearable
+                @select="option => selected = option">
+                <template slot="empty">No results found</template>
+            </b-autocomplete>
+        </b-field>
+    <div>
+      <img alt="Vue logo" src="../assets/administrator.png" />
+    </div>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                data: [
+                    'Ariana Grande',
+                    'Bruno Mars',
+                    'The Weeknd',
+                    'Floyd Mayweather',
+                    'Bernie Sanders',
+                    'Summer Walker',
+                    'Leonardo DiCaprio',
+                    'Billie Eilish',
+                    'Jhene Aiko',
+                    'Dolly Parton',
+                    'Chris Stapleton',
+                    'Axl Rose'
+                ],
+                name: '',
+                selected: null
+            }
+        },
+        computed: {
+            filteredDataArray() {
+                return this.data.filter((option) => {
+                    return option
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(this.name.toLowerCase()) >= 0
+                })
+            }
+        }
+    }
+</script>
